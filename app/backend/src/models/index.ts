8f8190eac6cb864eb69edf6ff683df5c844aafdf
Thiago <thiaguinhoguedes2006@gmail.com>
2026-05-22 22:01:48 -0300

    fixing backend connection and footer styling

--- app/backend/src/models/index.ts
+++ app/backend/src/models/index.ts
@@ -4,11 +4,5 @@ import Node from './Node.js';
 import Inquiry from './Inquiry.js';
 import InteractionLog from './InteractionLog.js';
 
-Inquiry.belongsTo(User, { foreignKey: 'answeredBy', as: 'answerer' });
-User.hasMany(Inquiry, { foreignKey: 'answeredBy', as: 'answeredInquiries' });
-
-InteractionLog.belongsTo(Inquiry, { foreignKey: 'inquiryId', as: 'inquiry' });
-Inquiry.hasMany(InteractionLog, { foreignKey: 'inquiryId', as: 'interactionLogs' });
-
 export { sequelize, connectWithRetry, User, Node, Inquiry, InteractionLog };
 export default sequelize;
